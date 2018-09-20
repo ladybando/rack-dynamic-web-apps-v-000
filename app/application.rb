9ff079fa19800f71b3eb98a2207be77589e35087
Ayana Bando <ayanaflax@yahoo.com>
2018-09-20 18:00:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,9 @@ class Application
   end
   
   def iterator
-    
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
